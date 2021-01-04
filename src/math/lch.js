19ea3a35d6f1c88c23f2a6dd9b22b2d0c825a105
kotoricode <fuzzfas@gmail.com>
2021-01-05 01:54:22 +0200

    lch

--- src/math/lch.js
+++ src/math/lch.js
@@ -1,6 +1,5 @@
 // https://www.w3.org/TR/css-color-4/#color-conversion-code
 // http://www.brucelindbloom.com/index.html?Math.html
-import { SettableArray } from "../settable-array";
 import { clamp, DEG_TO_RAD } from "../util";
 
 const cbEpsilon = 6 / 29;
@@ -21,29 +20,28 @@ const invFunc = (value) =>
     return (value > cbEpsilon) ? value**3 : iKappa*(116*value - 16);
 };
 
-export class Lch extends SettableArray
+export class Lch
 {
     constructor(l=0, c=0, h=0)
     {
-        super();
-        this.l = l;
-        this.c = c;
-        this.h = h;
+        this.lch = [l, c, h];
+        this.rgb = new Array(3);
+
+        this.generateRgb();
     }
 
-    toRgb()
+    generateRgb()
     {
-        const fy = (this.l + 16) / 116,
-              hRad = this.h * DEG_TO_RAD;
+        const fy = (this.lch[0] + 16) / 116,
+              c = this.lch[1],
+              hRad = this.lch[2] * DEG_TO_RAD;
 
-        const x = invFunc(fy + Math.cos(hRad) * this.c / 500) * 0.950489,
+        const x = invFunc(fy + Math.cos(hRad) * c / 500) * 0.950489,
               y = (this.l > 8) ? fy**3 : iKappa*this.l,
-              z = invFunc(fy - Math.sin(hRad) * this.c / 200) * 1.08884;
+              z = invFunc(fy - Math.sin(hRad) * c / 200) * 1.08884;
 
-        this.set(
-            lin2rgb(3.2406255*x - 1.537208*y - 0.4986286*z),
-            lin2rgb(-0.9689307*x + 1.8757561*y + 0.0415175*z),
-            lin2rgb(0.0557101*x - 0.2040211*y + 1.0569959*z)
-        );
+        this.rgb[0] = lin2rgb(3.2406255*x - 1.537208*y - 0.4986286*z);
+        this.rgb[1] = lin2rgb(-0.9689307*x + 1.8757561*y + 0.0415175*z);
+        this.rgb[2] = lin2rgb(0.0557101*x - 0.2040211*y + 1.0569959*z);
     }
 }
