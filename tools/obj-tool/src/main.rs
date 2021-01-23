962972e4bebf857b6e78fc659e1ce2274cd7190b
kotoricode <fuzzfas@gmail.com>
2021-01-24 01:09:16 +0200

    rusty

--- tools/obj-tool/src/main.rs
+++ tools/obj-tool/src/main.rs
@@ -7,56 +7,49 @@ const FACE: &str = "f";
 
 fn main() -> Result<()>
 {   
-    let mut out: Vec<String> = Vec::new();
-
-    for file in fs::read_dir("obj")?
+    for in_file in fs::read_dir("obj")?
     {
-        out.push(String::from("["));
+        let mut vertices = Vec::new();
+        let mut out = Vec::new();
+        out.push("[");
 
-        let path = file?.path();
-        let contents = fs::read_to_string(path)?;
+        let contents = fs::read_to_string(in_file?.path())?;
+        let lines = contents.lines();
 
-        let mut vertices = vec![];
-
-        for line in contents.lines()
+        for line in lines
         {
-            let parts: Vec<String> = line.split_whitespace()
-                                         .map(String::from)
-                                         .collect::<Vec<String>>();
-
-            let line_type = &parts[0];
+            let parts = line.split_whitespace().collect::<Vec<&str>>();
+            let line_type = parts[0];
 
             if line_type == VERTEX
             {
-                let x = parts[1].clone();
-                let y = parts[2].clone();
-                let z = parts[3].clone();
-
-                vertices.push([x, y, z]);
+                vertices.push([
+                    parts[1], parts[2], parts[3]
+                ]);
             }
             else if line_type == FACE
             {
                 for i in 1..4
                 {
-                    let v = &vertices[parts[i].parse::<usize>().unwrap() - 1];
+                    let idx = parts[i].parse::<usize>().unwrap() - 1;
+                    let v = vertices[idx];
 
                     for j in 0..3
                     {
-                        let value = v[j].clone();
-
-                        out.push(value);
-                        out.push(String::from(", "));
+                        out.push(v[j]);
+                        out.push(", ");
                     }
                 }
             }
         }
 
-        out.push(String::from("]"));
+        out.push("]");
 
-        let mut file = fs::File::create("test.txt")?;                                                                                                          
+        let mut out_file = fs::File::create("test.txt")?;      
+                                                                                                    
         for s in &out
         {
-            file.write(s.as_bytes())?;
+            out_file.write(s.as_bytes())?;
         }   
     }
 
