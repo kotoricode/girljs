3e288fe1285216e0333cd12c9a128702ba9778a5
kotoricode <fuzzfas@gmail.com>
2021-02-17 16:18:40 +0200

    move stuff to init

--- src/gl/program.js
+++ src/gl/program.js
@@ -37,6 +37,104 @@ export class Program
 
     static init()
     {
+        class Shader
+        {
+            constructor(src, uBlocks, uniforms)
+            {
+                this.src = src;
+                this.uBlocks = uBlocks;
+                this.uniforms = uniforms;
+            }
+        }
+
+        class FShader extends Shader
+        {
+            constructor(src, uBlocks, uniforms)
+            {
+                super(src, uBlocks, uniforms);
+                Object.freeze(this);
+            }
+        }
+
+        class VShader extends Shader
+        {
+            constructor(src, aLayout, uBlocks, uniforms)
+            {
+                super(src, uBlocks, uniforms);
+                this.aLayout = aLayout;
+                Object.freeze(this);
+            }
+        }
+
+        const VS_DEBUG = "VS_DEBUG";
+        const VS_UI = "VS_UI";
+        const VS_WORLD = "VS_WORLD";
+
+        const FS_DEBUG = "FS_DEBUG";
+        const FS_LUMA = "FS_LUMA";
+        const FS_TEXTURED = "FS_TEX";
+
+        const vertDef = new SafeMap([
+            [VS_DEBUG, new VShader(
+                vsDebugSrc,
+                new SafeMap([
+                    [$.A_POSITION, 3]
+                ]),
+                [$.UB_CAMERA]
+            )],
+
+            [VS_UI, new VShader(
+                vsUiSrc,
+                new SafeMap([
+                    [$.A_POSITION, 3],
+                    [$.A_TEXCOORD, 2]
+                ]),
+                null,
+                new Map([
+                    [U_TYPE_M4FV, new SafeMap([
+                        [$.U_TRANSFORM, [0, 0]]
+                    ])]
+                ])
+            )],
+
+            [VS_WORLD, new VShader(
+                vsWorldSrc,
+                new SafeMap([
+                    [$.A_POSITION, 3],
+                    [$.A_TEXCOORD, 2]
+                ]),
+                [$.UB_CAMERA],
+                new Map([
+                    [U_TYPE_M4FV, new SafeMap([
+                        [$.U_TRANSFORM, [0, 0]]
+                    ])]
+                ])
+            )]
+        ]);
+
+        const fragDef = new SafeMap([
+            [FS_DEBUG, new FShader(fsDebugSrc)],
+
+            [FS_LUMA, new FShader(fsLumaSrc)],
+
+            [FS_TEXTURED, new FShader(
+                fsTexturedSrc,
+                null,
+                new Map([
+                    [U_TYPE_4F, new SafeMap([
+                        [$.U_COLOR, [1, 1, 1, 1]]
+                    ])]
+                ])
+            )]
+        ]);
+
+        const programDef = [
+            $.PRG_DEBUG, VS_DEBUG, FS_DEBUG,
+            $.PRG_IMAGE, VS_UI,    FS_LUMA,
+            $.PRG_UI,    VS_UI,    FS_TEXTURED,
+            $.PRG_WORLD, VS_WORLD, FS_TEXTURED,
+        ];
+
         compiledPrograms.clear();
 
         for (let i = 0; i < programDef.length;)
@@ -139,147 +237,6 @@ export class Program
     }
 }
 
-let activeGlProgram;
-
-/*------------------------------------------------------------------------------
-    Program creation
-------------------------------------------------------------------------------*/
-const createAttachShader = (program, shaderId, shaderDef) =>
-{
-    const shader = gl.createShader(shaderId);
-    gl.shaderSource(shader, shaderDef.src);
-    gl.compileShader(shader);
-    gl.attachShader(program, shader);
-
-    return shader;
-};
-
-const detachDeleteShader = (program, shader) =>
-{
-    gl.detachShader(program, shader);
-    gl.deleteShader(shader);
-};
-
-/*------------------------------------------------------------------------------
-    Shader definition creation
-------------------------------------------------------------------------------*/
-class Shader
-{
-    constructor(src, uBlocks, uniforms)
-    {
-        this.src = src;
-        this.uBlocks = uBlocks;
-        this.uniforms = uniforms;
-    }
-}
-
-class FShader extends Shader
-{
-    constructor(src, uBlocks, uniforms)
-    {
-        super(src, uBlocks, uniforms);
-        Object.freeze(this);
-    }
-}
-
-class VShader extends Shader
-{
-    constructor(src, aLayout, uBlocks, uniforms)
-    {
-        super(src, uBlocks, uniforms);
-        this.aLayout = aLayout;
-        Object.freeze(this);
-    }
-}
-
-/*------------------------------------------------------------------------------
-    Const
-------------------------------------------------------------------------------*/
-const VS_DEBUG = "VS_DEBUG";
-const VS_UI = "VS_UI";
-const VS_WORLD = "VS_WORLD";
-
-const FS_DEBUG = "FS_DEBUG";
-const FS_LUMA = "FS_LUMA";
-const FS_TEXTURED = "FS_TEX";
-
-const U_TYPE_2F = "U_TYPE_2F";
-const U_TYPE_4F = "U_TYPE_4F";
-const U_TYPE_M4FV = "U_TYPE_M4FV";
-
-/*------------------------------------------------------------------------------
-    Vertex shader definitions
-------------------------------------------------------------------------------*/
-const vertDef = new SafeMap([
-    [VS_DEBUG, new VShader(
-        vsDebugSrc,
-        new SafeMap([
-            [$.A_POSITION, 3]
-        ]),
-        [$.UB_CAMERA]
-    )],
-
-    [VS_UI, new VShader(
-        vsUiSrc,
-        new SafeMap([
-            [$.A_POSITION, 3],
-            [$.A_TEXCOORD, 2]
-        ]),
-        null,
-        new Map([
-            [U_TYPE_M4FV, new SafeMap([
-                [$.U_TRANSFORM, [0, 0]]
-            ])]
-        ])
-    )],
-
-    [VS_WORLD, new VShader(
-        vsWorldSrc,
-        new SafeMap([
-            [$.A_POSITION, 3],
-            [$.A_TEXCOORD, 2]
-        ]),
-        [$.UB_CAMERA],
-        new Map([
-            [U_TYPE_M4FV, new SafeMap([
-                [$.U_TRANSFORM, [0, 0]]
-            ])]
-        ])
-    )]
-]);
-
-/*------------------------------------------------------------------------------
-    Fragment shader definitions
-------------------------------------------------------------------------------*/
-const fragDef = new SafeMap([
-    [FS_DEBUG, new FShader(fsDebugSrc)],
-
-    [FS_LUMA, new FShader(fsLumaSrc)],
-
-    [FS_TEXTURED, new FShader(
-        fsTexturedSrc,
-        null,
-        new Map([
-            [U_TYPE_4F, new SafeMap([
-                [$.U_COLOR, [1, 1, 1, 1]]
-            ])]
-        ])
-    )]
-]);
-
-/*------------------------------------------------------------------------------
-    Program definitions
-------------------------------------------------------------------------------*/
-const programDef = [
-    $.PRG_DEBUG, VS_DEBUG, FS_DEBUG,
-    $.PRG_IMAGE, VS_UI,    FS_LUMA,
-    $.PRG_UI,    VS_UI,    FS_TEXTURED,
-    $.PRG_WORLD, VS_WORLD, FS_TEXTURED,
-];
-
-/*------------------------------------------------------------------------------
-    Create and prepare programs
-------------------------------------------------------------------------------*/
 class CompiledProgram
 {
     constructor(vert, frag)
@@ -303,21 +260,35 @@ class CompiledProgram
 
     createGlProgram(vert, frag)
     {
-        const glProgram = gl.createProgram();
-        const vs = createAttachShader(glProgram, $.VERTEX_SHADER, vert);
-        const fs = createAttachShader(glProgram, $.FRAGMENT_SHADER, frag);
-        gl.linkProgram(glProgram);
+        this.glProgram = gl.createProgram();
+        const vs = this.createAttachShader($.VERTEX_SHADER, vert);
+        const fs = this.createAttachShader($.FRAGMENT_SHADER, frag);
+        gl.linkProgram(this.glProgram);
 
-        if (!gl.getProgramParameter(glProgram, $.LINK_STATUS))
+        if (!gl.getProgramParameter(this.glProgram, $.LINK_STATUS))
         {
             console.log(gl.getError());
-            throw glProgram;
+            throw this.glProgram;
         }
 
-        detachDeleteShader(glProgram, vs);
-        detachDeleteShader(glProgram, fs);
+        this.detachDeleteShader(vs);
+        this.detachDeleteShader(fs);
+    }
 
-        this.glProgram = glProgram;
+    createAttachShader(shaderId, shaderDef)
+    {
+        const shader = gl.createShader(shaderId);
+        gl.shaderSource(shader, shaderDef.src);
+        gl.compileShader(shader);
+        gl.attachShader(this.glProgram, shader);
+
+        return shader;
+    }
+
+    detachDeleteShader(shader)
+    {
+        gl.detachShader(this.glProgram, shader);
+        gl.deleteShader(shader);
     }
 
     createUniformBlocks(shader)
@@ -386,3 +357,8 @@ class CompiledProgram
 }
 
 const compiledPrograms = new SafeMap();
+let activeGlProgram;
+
+const U_TYPE_2F = "U_TYPE_2F";
+const U_TYPE_4F = "U_TYPE_4F";
+const U_TYPE_M4FV = "U_TYPE_M4FV";
