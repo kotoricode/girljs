6b88cc95d999ae7eb1da16073b50714238923076
kotoricode <fuzzfas@gmail.com>
2021-03-11 18:46:02 +0200

    -

--- src/processes/process-camera.js
+++ src/processes/process-camera.js
@@ -7,10 +7,9 @@ import { Ground } from "../components/ground";
 
 export const processCamera = () =>
 {
+    const [ground] = Scene.one($.ENT_GROUND, Ground);
     const ray = Camera.getRay();
     ray.fromMouse();
-
-    const [ground] = Scene.one($.ENT_GROUND, Ground);
     ray.collideGround(ground);
 
     const focusedEntityId = Camera.getFocusEntityId();
